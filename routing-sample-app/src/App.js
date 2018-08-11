33a1844509d98a2790bb8376efdbc1be774127f8
İbrahim <ibrahimdagdelentr@gmail.com>
2018-08-11 12:44:08 +0300

    adding routing app

---
+++ routing-sample-app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+/**
+ * Browser Router'ı App.js veya index.js 'de kullanabilir. Tüm child componentleri alacak 
+ * şekilde tanımlamak için.
+ */
+
+import Blog from './containers/Blog/Blog';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Blog />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
