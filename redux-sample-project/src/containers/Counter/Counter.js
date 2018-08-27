1e58a1a0a21a8170d01df2cbcb2e8776fbecd35b
İbrahim <ibrahimdagdelentr@gmail.com>
2018-08-27 23:22:40 +0300

    adding

--- redux-sample-project/src/containers/Counter/Counter.js
+++ redux-sample-project/src/containers/Counter/Counter.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'; //hoc
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionType from '../../store/actions';
 
 class Counter extends Component {
 
@@ -14,6 +15,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
                 <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
                 <CounterControl label="Subtract 8" clicked={this.props.onSubtractCounter} />
+                <hr></hr>
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -22,16 +30,19 @@ class Counter extends Component {
 const mapStateToProps = state => {
     return {
         //Bu continerdaki counter verisine erişmek için ctr'a yetki verdik.
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
-        onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
-        onAddCounter: () => dispatch({ type: 'ADD' }),
-        onSubtractCounter: () => dispatch({ type: 'SUBTRACT' }),
+        onIncrementCounter: () => dispatch({ type: actionType.INCREMENT }),
+        onDecrementCounter: () => dispatch({ type: actionType.DECREMENT }),
+        onAddCounter: () => dispatch({ type: actionType.ADD, val: 10 }),
+        onSubtractCounter: () => dispatch({ type: actionType.SUBTRACT, val: 8 }),
+        onStoreResult: () => dispatch({ type: actionType.STORE_RESULT }),
+        onDeleteResult: (id) => dispatch({ type: actionType.DELETE_RESULT, resultElId: id })
     };
 }
 
--- redux-sample-project/src/store/reducer.js
+++ redux-sample-project/src/store/reducer.js
@@ -1,28 +1,54 @@
+import * as actionTypes from './actions';
+
 const initialState = {
-    counter: 5
-}
+    counter: 5,
+    results: []
+};
 
 const reducer = (state = initialState, action) => {
 
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
-    }
-    if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if (action.type === 'ADD') {
-        return {
-            counter: state.counter + 10
-        }
-    }
-    if (action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - 8
-        }
+    switch (action.type) {/*
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }*/
+        case actionTypes.INCREMENT:
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1;
+            return newState;
+        case actionTypes.DECREMENT:
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        case actionTypes.ADD:
+            return {
+                ...state,
+                counter: state.counter + action.val
+            }
+        case actionTypes.SUBTRACT:
+            return {
+                ...state,
+                counter: state.counter - action.val
+            }
+        case actionTypes.STORE_RESULT:
+            return {
+                ...state,
+                //@concat
+                //Bu yöntem mevcut dizileri değiştirmez, ancak birleştirilen dizilerin değerlerini içeren yeni bir dizi döndürür.
+                results: state.results.concat({ id: new Date(), value: state.counter })
+            }
+        case actionTypes.DELETE_RESULT:
+            // const id = 2;
+            // const newArray = [...state.results];
+            // newArray.splice(id,1);
+
+            //updating array
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            return {
+                ...state,
+                results:updatedArray
+            }
     }
 
     return state;
