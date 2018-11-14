9eb9237507bba73c32e6b7e681f21f441641670a
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2018-11-14 14:54:42 +0300

    Setup a project with uisng next.js

---
+++ nextjs-sample-app/pages/auth/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const authIndexPage = () => (
+    <div>
+        <h1>The Auth Index Page</h1>
+        <a href="/">Auth</a>
+    </div>
+);
+
+export default authIndexPage;
---
+++ nextjs-sample-app/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const indexPage = () => (
+    <div>
+        <h1>Main Page</h1>
+        <a href="/auth">Auth</a>
+    </div>
+);
+
+export default indexPage;
