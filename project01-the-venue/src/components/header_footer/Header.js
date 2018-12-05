667918d51fbcace5325af821a3a460b9c2dc2312
İbrahim DAGDELEN <ibrahimdagdelentr@gmail.com>
2018-12-05 16:02:16 +0300

    created the header

--- project01-the-venue/src/components/header_footer/Header.js
+++ project01-the-venue/src/components/header_footer/Header.js
@@ -18,7 +18,10 @@ class Header extends Component {
                     }}
                 >
                 <ToolBar>
-                    <div className="header_logo"></div>
+                    <div className="header_logo">
+                        <div className="font_righteous header_logo_venue">The Venue</div>
+                        <div className="header_logo_title">Musical Events</div>
+                    </div>
                 </ToolBar>
                 </AppBar>
             </div>
