5ce2477c33c8279c96c35c5458998c4e9668af29
İbrahim DAGDELEN <ibrahimdagdelentr@gmail.com>
2018-12-05 15:55:18 +0300

    The venue project initialite

---
+++ project01-the-venue/src/components/header_footer/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/core/Menu';
+import IconButton from '@material-ui/core/IconButton'
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <AppBar
+                    position="fixed"
+                    style={{
+                        backgroundColor:'#2f2f2f',
+                        boxShadow:'none',
+                        padding:'10px 0px'
+                    }}
+                >
+                <ToolBar>
+                    <div className="header_logo"></div>
+                </ToolBar>
+                </AppBar>
+            </div>
+        );
+    }
+}
+
+export default Header;
