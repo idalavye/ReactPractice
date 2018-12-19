4a60d2d077a25e7ea014893e907a4097c93f7aed
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2018-12-19 17:14:30 +0300

    added a header

---
+++ project02-man-city/src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './hoc/Layout';
+
+const Routes = (props) => {
+  return(
+    <div>
+      <Layout>
+        Second Line
+      </Layout>
+    </div>
+  )
+}
+
+export default Routes;
