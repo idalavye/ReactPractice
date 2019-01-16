5b53cd429eb35e958023ee163af88ef51c9fdb4b
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-01-16 19:00:54 +0300

    Home: Finished player card

--- project02-man-city/src/components/home/meetPlayers/cards.js
+++ project02-man-city/src/components/home/meetPlayers/cards.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { easePolyOut } from 'd3-ease';
 import Animate from 'react-move/Animate';
 import Otamendi from '../../../Resources/images/players/Otamendi.png';
-
+import PlayerCard from '../../ui/playerCard';
 
 class HomeCards extends Component {
 
     state = {
-        cards:[
+        cards: [
             {
                 bottom: 90,
                 left: 300
@@ -28,24 +28,24 @@ class HomeCards extends Component {
     }
 
     showAnimateCards = () => (
-        this.state.cards.map((card,i)=>(
+        this.state.cards.map((card, i) => (
             <Animate
                 key={i}
                 show={this.props.show}
 
                 start={{
-                    left:0,
-                    bottom:0
+                    left: 0,
+                    bottom: 0
                 }}
 
                 enter={{
                     left: [card.left],
                     bottom: [card.bottom],
-                    timing: {duration: 500, ease: easePolyOut}
+                    timing: { duration: 500, ease: easePolyOut }
                 }}
             >
-                {({ left , bottom })=>{
-                    return(
+                {({ left, bottom }) => {
+                    return (
                         <div
                             style={{
                                 position: 'absolute',
@@ -53,7 +53,11 @@ class HomeCards extends Component {
                                 bottom
                             }}
                         >
-                            Div
+                            <PlayerCard
+                                number={30}
+                                name="Nicolas"
+                                lastName="Otomendi"
+                                bck={Otamendi} />
                         </div>
                     )
                 }}
