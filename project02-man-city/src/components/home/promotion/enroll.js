f354fbd27d9421298955123b60c8934d20686300
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-01-17 22:34:33 +0300

    Enroll: created

---
+++ project02-man-city/src/components/home/promotion/enroll.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class enroll extends Component {
+    render() {
+        return (
+            <div>
+                enroll component
+            </div>
+        );
+    }
+}
+
+export default enroll;
