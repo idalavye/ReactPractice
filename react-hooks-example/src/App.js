d28509f58679b4de336ab58990079d18ff8d8469
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-01-21 22:25:28 +0300

    the useContext() hook

--- react-hooks-example/src/App.js
+++ react-hooks-example/src/App.js
@@ -3,20 +3,29 @@ import React, { useState } from 'react';
 import Todo from './components/Todo';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import AuthContext from './auth-context';
 
 const app = props => {
 
   const [page, setPage] = useState('auth');
+  const [authStatus, setAuthStatus] = useState(false);
 
   const switchPage = (pageName) => {
     setPage(pageName);
   }
 
+  const loginFunc = () => {
+    setAuthStatus(true);
+  }
+
   return (
     <div className="App">
-      <Header onLoadTodos={switchPage.bind(this, 'todos')} onLoadAuth={switchPage.bind(this, 'auth')} />
-      <hr />
-      {page === 'auth' ? <Auth /> : <Todo />}
+      <AuthContext.Provider value={{ status: authStatus, login: loginFunc }}>
+        <Header onLoadTodos={switchPage.bind(this, 'todos')} onLoadAuth={switchPage.bind(this, 'auth')} />
+        <hr />
+        {page === 'auth' ? <Auth /> : <Todo />}
+
+      </AuthContext.Provider>
     </div>
   )
 };
--- react-hooks-example/src/components/Auth.js
+++ react-hooks-example/src/components/Auth.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import AuthContext from '../auth-context';
 
 const Auth = () => {
+    const auth = useContext(AuthContext);
+
     return (
         <div>
-            <h>Auth Component</h>
+            <button onClick={auth.login}>Log in!</button>
         </div>
     );
 };
