54187f9d3cfc026658db1cbe967a9533b1b2861a
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-01-21 22:08:50 +0300

    converting app component to hook based component

--- react-hooks-example/src/App.js
+++ react-hooks-example/src/App.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Todo from './components/Todo';
+import Header from './components/Header';
+import Auth from './components/Auth';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Todo />
-      </div>
-    );
+const app = props => {
+
+  const [page, setPage] = useState('auth');
+
+  const switchPage = (pageName) => {
+    setPage(pageName);
   }
-}
 
-export default App;
+  return (
+    <div className="App">
+      <Header onLoadTodos={switchPage.bind(this, 'todos')} onLoadAuth={switchPage.bind(this, 'auth')} />
+      <hr />
+      {page === 'auth' ? <Auth /> : <Todo />}
+    </div>
+  )
+};
+
+export default app;
---
+++ react-hooks-example/src/components/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Auth = () => {
+    return (
+        <div>
+            <h>Auth Component</h>
+        </div>
+    );
+};
+
+export default Auth;
